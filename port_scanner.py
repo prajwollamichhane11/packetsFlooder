278a0a914429f529c5f5556d5ef6114d371d32d1
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-29 18:44:25 +0545

    The ultimate open port scanner

---
+++ port_scanner.py
@@ -0,0 +1,29 @@
+import socket
+import argparse
+from contextlib import closing
+
+def check_socket(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex((host, port)) == 0:
+            print ("Port is open")
+            return 1
+        else:
+            print ("Port is not open")
+            return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="A Port Scanner to Scan the host")
+    print("Usage: <python3> <filename> <host>")
+    parser.add_argument('host', nargs="?", help="Host to perform stress test on")
+    args = parser.parse_args()
+
+    final_port = int(input("Enter the port you want to search to: "))
+
+    for socket_count in range(801,final_port):
+        print (socket_count)
+        value = check_socket(args.host,socket_count)
+        if value == 1:
+            open_port = socket_count
+            break
+        if value == 0:
+            continue
