a58dc66b7b50a733fb463a90d2ddded3811b6eae
prajwollamichhane11 <prajwollamichhane11@gmail.com>
2019-04-29 18:54:45 +0545

    ---

--- port_scanner.py
+++ port_scanner.py
@@ -12,19 +12,23 @@ def check_socket(host, port):
             return 0
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description="A Port Scanner to Scan the host")
-    print("Usage: <python3> <filename> <host>")
     parser.add_argument('host', nargs="?", help="Host to perform stress test on")
     args = parser.parse_args()
 
-    final_port = int(input("Enter the port you want to search to: "))
 
-    for socket_count in range(801,final_port):
-        print('---------------------------')
-        print (socket_count)
-        value = check_socket(args.host,socket_count)
-        if value == 1:
-            open_port = socket_count
-            break
-        if value == 0:
-            continue
+    if args.host == True:
+        final_port = int(input("Enter the port you want to search to: "))
+
+        for socket_count in range(801,final_port):
+            print('---------------------------')
+            print (socket_count)
+            value = check_socket(args.host,socket_count)
+            if value == 1:
+                open_port = socket_count
+                break
+            if value == 0:
+                continue
+    else:
+        print("Usage: <python3> <filename> <Host Address OR Website>")
